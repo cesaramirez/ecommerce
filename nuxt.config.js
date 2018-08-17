4e43570492ac868baa674d708af14aa43e7f0b47
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-08-16 20:08:22 -0600

    :sparkles: Add Bulma CSS Framework

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,9 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  css: [
+    '~assets/styles/app.scss'
+  ],
   /*
   ** Customize the progress bar color
   */
@@ -24,16 +27,5 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
   }
 }
-
